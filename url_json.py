657d96cb9b5dfcf645e8f5cc185eecccc90dadd2
Ubuntu <ubuntu@ip-172-31-0-248.us-west-1.compute.internal>
2022-06-27 05:49:23 +0000

    fixed add to table bug

--- url_json.py
+++ url_json.py
@@ -3,6 +3,7 @@ from urllib.request import urlopen
 import json
 import pymysql
 from datetime import datetime
+import sys
 
 host = 'database-2.cji0mokilyxv.us-west-1.rds.amazonaws.com'
 user = 'admin'
@@ -69,13 +70,13 @@ def get_mysql_cursor():
     use_database = "USE " + dataBaseName
     mycursor.execute(use_database)
 
-    return mycursor
+    return mycursor, connection
 
 # display a table
 def show_table(tableName, mycursor=None):
 
     if mycursor is None:
-        mycursor = get_mysql_cursor()
+        mycursor, connection = get_mysql_cursor()
 
     show_tables = "SELECT * FROM " + tableName
     mycursor.execute(show_tables)
@@ -90,12 +91,14 @@ def show_table(tableName, mycursor=None):
 # Columns: addr VARCHAR(34), user VARCHAR(34)
 def add_address(address, user1):
 
-    mycursor = get_mysql_cursor()
+    mycursor, connection = get_mysql_cursor()
 
     add_address = 'INSERT INTO AddressBook (addr, user) VALUES ("' + address + '", "'+ user1 + '")'
     mycursor.execute(add_address)
+    connection.commit()
 
-    show_table('AddressBook', mycursor)
+    #show_table('AddressBook', mycursor)
+    show_table('AddressBook')
     
 # adds address and user to address book
 # Table: Balances
@@ -103,14 +106,15 @@ def add_address(address, user1):
 # DECIMAL
 def add_balance(address, balance_btc, balance_usd, datetime=None):
 
-    mycursor = get_mysql_cursor()
+    mycursor, connection = get_mysql_cursor()
     if datetime is None:
         datetime = get_datetime()
 
     add_balance = 'INSERT INTO Balances (addr, time, balance_usd, balance_btc) VALUES ("' + address + '", "'+ datetime + '", '+ str(balance_usd) + ',' + str(balance_btc) + ')'
     mycursor.execute(add_balance)
+    connection.commit()
 
-    show_table('Balances', mycursor)
+    show_table('Balances')
 
 # adds address and user to address book
 # Table: Transactions
@@ -118,8 +122,9 @@ def add_balance(address, balance_btc, balance_usd, datetime=None):
 # DECIMAL
 def add_transaction(address, transData, mycursor=None, show=True):
 
+    connection = None
     if mycursor is None:
-        mycursor = get_mysql_cursor()
+        mycursor, connection = get_mysql_cursor()
     block_id = transData['block_id']
     hash1    = transData['hash']
     time     = transData['time']
@@ -127,6 +132,9 @@ def add_transaction(address, transData, mycursor=None, show=True):
 
     add_transaction= 'INSERT INTO Transactions (addr, hash, time, balance_change, block_id) VALUES ("'+ address + '", "'+ hash1 + '", "'+ time + '", ' + str(balance_change) + ', ' + str(block_id) + ')'
     mycursor.execute(add_transaction)
+    
+    if connection is not None:
+        connection.commit()
 
     if show:
         show_table('Transactions', mycursor)
@@ -138,17 +146,33 @@ def add_n_transactions(address, data_dict, n=None):
     if n is None:
         n = len(data_dict['transactions'])
 
-    mycursor = get_mysql_cursor()
+    mycursor, connection = get_mysql_cursor()
     for i in range(n):
         add_transaction(address, data_dict['transactions'][i], mycursor,
                 show=False)
 
-    show_table('Transactions', mycursor)
+    #show_table('Transactions', mycursor)
+    connection.commit()
+    mycursor.close()
 
 
 # from the address book return all addresses
-def get_addresses():
-    pass
+def get_addresses(user):
+    mycursor, connection = get_mysql_cursor()
+    
+    get_addr_query = 'SELECT addr FROM AddressBook WHERE user="'+ user + '";'
+    mycursor.execute(get_addr_query)
+    rows = mycursor.fetchall()
+
+    import pdb; pdb.set_trace()
+
+    for row in rows:
+        print('address ', row)
+
+    mycursor.close()
+
+    return "happy tree friends" 
+
 
 # from the address book, get the next address
 def get_next_address(iterator1):
@@ -164,12 +188,20 @@ def main():
     address = '3E8ociqZa9mZUSwGdSmAEMAoAxBK3FNDcd'
     user1 = "Bobert"
     add_address(address, user1)   
+    print("Address book contents:")
+    show_table("AddressBook")
+    print('===============')
     data_dict, data_address = get_data_address(url_base, address)
 
     datetime = data_dict['transactions'][0]['time']
     add_balance(address, data_dict['balance'], data_dict['balance_usd'], datetime)
     add_n_transactions(address, data_dict, 10)
     #add_transaction(address, data_dict['transactions'][-1])
+
+    addresses = get_addresses(user1)
+    print("Bobert's Addresses: ", addresses)
+
+    sys.exit(0)
 
     import pdb; pdb.set_trace()
     
